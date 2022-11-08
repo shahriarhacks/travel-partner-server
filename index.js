e7e7cbd3621e98e06a98a5aa2327cc610c1b94a4
Shahriar Ahmed <shahriar7ahmed@gmail.com>
2022-11-08 07:56:26 +0600

    mongodb/packages add

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
@@ -8,6 +9,35 @@ app.use(express.json());
 
 const port = process.env.PORT || 5000;
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.whzqc0b.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+async function run() {
+  try {
+    const servicesCollection = client
+      .db("TravelPartner")
+      .collection("packages");
+
+    app.get("/home/packages", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const services = await cursor.limit(3).toArray();
+      res.send(services);
+    });
+
+    app.get("/packages", async (req, res) => {
+      const query = {};
+      const cursor = servicesCollection.find(query);
+      const packages = await cursor.toArray();
+      res.send(packages);
+    });
+  } finally {
+  }
+}
+run().catch((err) => console.error(err));
 app.get("/", (req, res) => {
   res.send("Server is ready for fight");
 });
